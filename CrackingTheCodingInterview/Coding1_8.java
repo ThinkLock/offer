074094572d640be4d7988a7e3348e48bbfa13dc0
ThinkLock <18202669826@126.com>
2017-03-06 12:57:36 +0800

    3月6日上午
    数组与字符串

---
+++ CrackingTheCodingInterview/Coding1_8.java
@@ -0,0 +1,13 @@
+public class Coding1_8{
+
+  public static boolean isSubString(String s1; String s2){
+
+
+  }
+
+  public static void main(String[] args) {
+      String s1 = "waterbottlw";
+      String s2 = "";
+      System.out.println()
+  }
+}
